25d8b0f13cf95a5458ddc759f64ec168852d13ea
Pasquale Rocchio <patrocc6@gmail.com>
2017-04-30 12:20:43 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array[0] + " and " + array[1]
+  else
+    oxford_string = ""
+    array.each_with_index do |word, index|
+      if index == array.length-1
+        oxford_string << "and #{word}"
+      else
+        oxford_string << "#{word}, "
+      end
+    end
+    oxford_string
+  end
 end
